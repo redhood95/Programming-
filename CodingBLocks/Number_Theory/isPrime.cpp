06b418e43e28aa89e47b30414bc3548832f6f4cc
yash upadhyay <yashup1997@gmail.com>
2019-02-01 23:29:46 +0530

    sqaureroot method for finding prime

--- CodingBLocks/Number_Theory/isPrime.cpp
+++ CodingBLocks/Number_Theory/isPrime.cpp
@@ -18,13 +18,28 @@ for(int i=2;i*i<=a;i++){
 }
 return true;
 }
+bool op_is_prime(int a)
+{
+if(a==1){
+    return false;
+}
+if(a==2){
+    return true;
+}
+for(int i=3;i*i<=a;i=i+2){
+    if(a%i==0){
+        return false;
+    }
 
+}
+return true;
+}
 int main()
 {
     int n;
 
     cin>>n;
-    if(is_prime(n)){
+    if(op_is_prime(n)){
         cout<<"the number" << n << "is prime";
     }
     else{
