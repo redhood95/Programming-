3ac49f7c136c47aed7693bec57880b231896382a
yash upadhyay <yashup1997@gmail.com>
2019-02-01 17:07:33 +0530

    inbuit sort

--- CodingBLocks/STL/inbuiltSort.cpp
+++ CodingBLocks/STL/inbuiltSort.cpp
@@ -3,14 +3,27 @@
 
 using namespace std;
 
+bool comp(int a, int b){
+    return a>b;
+
+}
+
 int main()
 {
     int a[] = {1,4,2,4,2,8,7,4};
     int n = sizeof(a)/sizeof(int);
     sort(a,a+n);
     for(int i=0;i<=n;i++){
-        cout<<a[i]<< " ";   }
+        cout<<a[i]<< " ";
 
+        }
+        int b[] = {1,4,2,4,2,8,7,4};
+sort(b,b+n,comp);
+cout<<"\n";
+for(int i=0;i<=n;i++){
+        cout<<b[i]<< " ";
+
+        }
     return 0;
 
 
