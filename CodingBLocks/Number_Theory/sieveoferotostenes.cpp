4acf6ac8790254bb4789e4459ac9527195047b87
yash upadhyay <yashup1997@gmail.com>
2019-02-02 22:28:39 +0530

    prime seive

--- CodingBLocks/Number_Theory/sieveoferotostenes.cpp
+++ CodingBLocks/Number_Theory/sieveoferotostenes.cpp
@@ -2,27 +2,41 @@
 
 using namespace std;
 
-void primes(int *p)
-{
-    for(int i=2;i<=10000;i++)
-        p[i]=1;
 
-    for(int i=2;i<=10000;i++)
+
+void primes(int *p,int n)
+{
+    p[1]=0;
+        p[0]=0;
+        p[2]=1;
+    for(int i=3;i<=n;i++)
+      {
+          p[i]=1;
+
+      }
+    for(int i=3;i<=n;i+=2)
         {
         if(p[i])
             {
-            for(int j=2*i;j<=10000;j+=i){
+            for(int j=i*i;j<=n;j+=2*i){
                 p[j]=0;
             }
             }
 
         }
-        p[1]=0;
-        p[0]=0;
+return;
 }
+
 int main()
-
 {
-
+int N =1000;
+int p[N]={0};
+primes(p,100);
+for(int i=0;i<=100;i++)
+    {
+    if(p[i]){
+        cout<<i<<endl;
+    }
+    }
 return 0;
 }
