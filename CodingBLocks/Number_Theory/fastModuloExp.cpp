a06fe9abc227a943c7acb068c987213b02c9c33d
yash upadhyay <yashup1997@gmail.com>
2019-02-11 00:22:16 +0530

    fast modulo exponentiation

---
+++ CodingBLocks/Number_Theory/fastModuloExp.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+#define ll long long
+
+ll fastModulo(ll a,ll b, ll m)
+{
+ll res=1;
+while(b>0){
+    if(b&1){
+        res = (res*a)%m;
+    }
+    a = a*a%m;
+    b = b>>1;
+
+
+}
+return res;
+}
+
+int main()
+
+{
+    cout<<fastModulo(5,3,100);
+return 0;
+}
